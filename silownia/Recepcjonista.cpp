481fbaac8a8c72abfbaab7a27e89b294707c152b
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-08 16:51:49 +0100

    Update Recepcjonista.cpp

--- silownia/Recepcjonista.cpp
+++ silownia/Recepcjonista.cpp
@@ -4,21 +4,24 @@ using namespace std;
 
 #include "Recepcjonista.h"
 #include "Karnet.h"
-#include "Pracownik.h"
 
-void Recepcjonista::stw�rz_harmonogram() {
+void Recepcjonista::stwórz_harmonogram() 
+{
 	throw "Not yet implemented";
 }
 
-void Recepcjonista::modyfikuj_harmonogram() {
+void Recepcjonista::modyfikuj_harmonogram() 
+{
 	throw "Not yet implemented";
 }
 
-void Recepcjonista::archiwizuj_potwierdzenia_przelewu() {
-	throw "Not yet implemented";
+void Recepcjonista::archiwizuj_potwierdzenia_przelewu() 
+{
+	cout<<"Potwierdzenie zostało przesłane ";
 }
 
-void Recepcjonista::dodaj_karnet() {
+void Recepcjonista::dodaj_karnet() 
+{
 	throw "Not yet implemented";
 }
 
