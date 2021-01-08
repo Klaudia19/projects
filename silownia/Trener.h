65994457f63e782f69a087b7ac6020147a478604
Klaudia19 <77021636+Klaudia19@users.noreply.github.com>
2021-01-08 18:24:26 +0100

    Update Trener.h

--- silownia/Trener.h
+++ silownia/Trener.h
@@ -8,25 +8,23 @@ using namespace std;
 
 #include "Karta_zdrowia.h"
 #include "Grupa_zajêciowa.h"
-// #include "Wizyta_u_dietetyla.h"
 #include "Trening_personalny.h"
 #include "Pracownik.h"
 
 class Karta_zdrowia;
 class Grupa_zajêciowa;
 class Trening_personalny;
-// class Pracownik;
+class Pracownik;
 class Trener;
 
 class Trener: public Pracownik
 {
-	//public: std::vector<Karta_zdrowia*> _unnamed_Karta_zdrowia_;
 	public: std::vector<Grupa_zajêciowa*> _unnamed_Grupa_zajêciowa_;
 	public: std::vector<Trening_personalny*> _unnamed_Trening_personalny_;
 
 	public: void stwórz_plan_treningowy();
         public: void wglad_do_karty_zdrowia(Karta_zdrowia);
-	public: string dostępność_godzinowa();
+	public: int[70] dostępność_godzinowa();
 };
 
 #endif
